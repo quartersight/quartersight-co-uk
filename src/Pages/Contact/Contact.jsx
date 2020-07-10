88e7d613951631de0258b4acbae1f6c794e163e0
Carl Turner <carl@Carls-MacBook-Pro.local>
2020-07-10 21:35:11 +0100

    new content for projects and contact

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -9,7 +9,74 @@ class Contact extends Component {
   render() {
     return (
       <>
-        <h1>Contact</h1>
+        <div className="container main-content">
+          <div className="row">
+            <div className="col">
+              <section className="getintouch">
+                <div className="container modern-form">
+                  <div className="text-center">
+                    <h4>Get in touch</h4>
+                  </div>
+                  <hr className="modern-form__hr" />
+                  <div className="modern-form__form-container">
+                    <form>
+                      <div className="form-row">
+                        <div className="col col-contact">
+                          <div className="form-group modern-form__form-group--padding-r">
+                            <input
+                              className="form-control input input-tr"
+                              type="text"
+                              placeholder="Name"
+                              autoFocus
+                            />
+                            <div className="line-box">
+                              <div className="line"></div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col col-contact">
+                          <div className="form-group modern-form__form-group--padding-l">
+                            <input
+                              className="form-control input input-tr"
+                              type="text"
+                              placeholder="Email"
+                            />
+                            <div className="line-box">
+                              <div className="line"></div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="form-row">
+                        <div className="col">
+                          <div className="form-group modern-form__form-group--padding-t">
+                            <textarea
+                              className="form-control input modern-form__form-control--textarea"
+                              placeholder="Message"
+                            ></textarea>
+                            <div className="line-box">
+                              <div className="line"></div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="form-row">
+                        <div className="col text-center">
+                          <button
+                            className="btn btn-primary submit-now"
+                            type="submit"
+                          >
+                            Submit Now
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { FaArrowRight, FaGithub, FaGlobe, FaTimesCircle } from "react-icons/fa";
 
 class Projects extends Component {
   constructor(props) {
@@ -9,7 +10,116 @@ class Projects extends Component {
   render() {
     return (
       <>
-        <h1>Projects</h1>
+        <div class="container main-content">
+          <div class="row">
+            <div class="col">
+              <h1 class="text-center">Current and recent projects</h1>
+              <p>
+                While I don't have the most experience, I've been learning as
+                much as I can. Below are some of the projects that I have worked
+                on, and links to either the live product or the GitHub repo.
+              </p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Pident</h4>
+                  <h6 class="text-muted card-subtitle mb-2">
+                    Broadcast audio playout
+                  </h6>
+                  <p class="card-text">
+                    A simple solution to an expensive problem. Rather than
+                    replacing broken equipment with more outdated tech, I built
+                    a simple touchscreen solution for playing out various audio
+                    furniture used in local regional news.
+                  </p>
+                  <a
+                    class="card-link"
+                    href="http://www.github.com/quartersight/pident"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    View on GitHub&nbsp;
+                    <FaArrowRight />
+                    <FaGithub />
+                  </a>
+                </div>
+              </div>
+              <hr />
+            </div>
+            <div class="col-md-4">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Breathe Serenity</h4>
+                  <h6 class="text-muted card-subtitle mb-2">
+                    Simple static site build
+                  </h6>
+                  <p class="card-text">
+                    Another simple static site built for a hypnotherapist in the
+                    south east.
+                  </p>
+                  <a
+                    class="card-link"
+                    href="http://www.breatheserenity.co.uk"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Visit the site&nbsp;
+                    <FaArrowRight />
+                    <FaGlobe />
+                  </a>
+                </div>
+              </div>
+              <hr />
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Oyster Create</h4>
+                  <h6 class="text-muted card-subtitle mb-2">
+                    Web design and build
+                  </h6>
+                  <p class="card-text">
+                    A fully designed and hosted website for a creative design
+                    agency. And yes, I might have a hand in that firm...
+                  </p>
+                  <a
+                    class="card-link"
+                    href="http://www.oystercreate.co.uk"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Visit the site&nbsp;
+                    <FaArrowRight />
+                    <FaGlobe />
+                  </a>
+                </div>
+              </div>
+              <hr />
+            </div>
+            <div class="col-md-4">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Pident 2</h4>
+                  <h6 class="text-muted card-subtitle mb-2">
+                    Broadcast audio playout
+                  </h6>
+                  <p class="card-text">
+                    An updated version which has more commercial potential and
+                    scalability. Physical interfaces, a web client for managing
+                    audio and a nice rack mounted unit are some of the
+                    improvements this version offers. This is an ongoing
+                    project.
+                  </p>
+                  No public code, sorry!&nbsp;
+                  <FaArrowRight />
+                  <FaTimesCircle />
+                </div>
+              </div>
+              <hr />
+            </div>
+          </div>
+        </div>
       </>
     );
   }
